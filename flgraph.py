64fd10d246e19c33f324a9acf5017789cabe29cc
Shashank Kambhampati <frost263@gmail.com>
2017-11-01 12:29:53 -0500

    Fixed indenting

--- flgraph.py
+++ flgraph.py
@@ -2,45 +2,44 @@ import numpy as np
 
 class fgraph:
     f_len = None
-        c_len = None
-        _fcosts = None
-        _ccosts = None
+    c_len = None
+    _fcosts = None
+    _ccosts = None
 
-        def __init__(self, fn):
-            f = open(fn)
+    def __init__(self, fn):
+       f = open(fn)
 
-                lengths = f.readline().split()
-                self.f_len = int(lengths[0])
-                self.c_len = int(lengths[1])
+       lengths = f.readline().split()
+       self.f_len = int(lengths[0])
+       self.c_len = int(lengths[1])
 
-                self._fcosts = map(float, f.readline().split())
+       self._fcosts = map(float, f.readline().split())
 
-                try:
-                    for i in xrange(0, self.c_len):
-                        client = map(float, f.readline().split())
-                                if self._ccosts is None:
-                                    self._ccosts = [client]
-                                else:
-                                    self._ccosts.append(client)
-                                if len(client) != self.f_len:
-                                    raise ValueError()
-                except ValueError as err:
-                    print "_fcosts and _ccosts must contain f_len values."
-                except Exception as err:
-                    print "Invalid Input File Format"
-                        print "Proper Format is"
-                        print "f_len c_len"
-                        print "[_fcosts]"
-                        print "[_ccosts][0]"
-                        print "[_ccosts][1]"
-                        print ". . ."
-                print "Constructor"
+       try:
+            for i in xrange(0, self.c_len):
+                client = map(float, f.readline().split())
+                if self._ccosts is None:
+                    self._ccosts = [client]
+                else:
+                    self._ccosts.append(client)
+                if len(client) != self.f_len:
+                    raise ValueError()
+       except ValueError as err:
+           print "_fcosts and _ccosts must contain f_len values."
+       except Exception as err:
+           print "Invalid Input File Format"
+           print "Proper Format is"
+           print "f_len c_len"
+           print "[_fcosts]"
+           print "[_ccosts][0]"
+           print "[_ccosts][1]"
+           print ". . ."
 
-        def c_flat(self):
-            return np.transpose(self._ccosts).flatten().tolist()
+    def c_flat(self):
+        return np.transpose(self._ccosts).flatten().tolist()
 
-        def getNeighbors(self, facility):
-            try:
-                neighbor = np.transpose(self._ccosts)[facility]
-            except IndexError as err:
-                print "fgraph instance contains " + str(f_len) + " facilities."
+    def getNeighbors(self, facility):
+        try:
+            neighbor = np.transpose(self._ccosts)[facility]
+        except IndexError as err:
+            print "fgraph instance contains " + str(f_len) + " facilities."
