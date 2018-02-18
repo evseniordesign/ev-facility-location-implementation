183440972d2d273ed9dfacaa5204b951c2da4c3a
Kevin Wu <kevin@kevinywu.com>
2018-02-17 22:48:01 -0600

    Add facility generator functionality

--- mapping.py
+++ mapping.py
@@ -12,6 +12,10 @@ not a priority.
 
 import json
 import math
+
+import numpy as np
+import pandas as pd
+
 from facility_location.algorithm import choose_facilities, Algorithms
 from cost_gen import get_fcost, get_ccost
 
@@ -36,7 +40,9 @@ def make_mapping(data, facility_func, client_func):
     return fcosts, ccosts
     
 
-def test_mapping(filename):
+# set "gen" to True if you'd like to use generated facility data based on
+# the datafile instead of the actual data
+def test_mapping(filename, gen = False):
     """
     A very simplistic mapping to the facility location problem.
     More of a proof of concept than an actual attempt.
@@ -49,8 +55,47 @@ def test_mapping(filename):
         #facility_func = lambda facility: 1000000
 
         #client_func = lambda client, facility: distance(client, facility) * 1000
+        if gen:
+            data['facilities'] = generate(data['facilities'])
 
         return make_mapping(data, get_fcost, get_ccost)
+
+def generate(facilities):
+    # locations = pd.read_csv(filename)
+    locations = pd.DataFrame(facilities)
+
+    centroid = (np.sum(locations.lat) / len(locations.lat), np.sum(locations.long) / len(locations.long))
+
+    lat_normalized = []
+    for i in locations.lat:
+        lat_normalized.append(locations.lat - centroid[0])
+
+    long_normalized = []
+    for i in locations.long:
+        long_normalized.append(locations.long - centroid[1])
+
+    cov = np.cov(lat_normalized, long_normalized)
+    # cov[0][0]
+    # cov[0][len(cov[0])/ 2] # len/2
+
+    # set this range to however many values you want to generate
+    out = []
+    for i in range(0,100):
+        vals = np.random.multivariate_normal(centroid, [[cov[0][0], cov[0][len(cov) / 2]],[cov[1][0], cov[1][len(cov) / 2]]])
+        out.append(
+            {
+                'lat': vals[0],
+                'long': vals[1]
+            }
+        )
+            
+
+
+    print("Generate out: ") 
+    print(out)
+    print("---")
+
+    return out
 
 def main():
     """
